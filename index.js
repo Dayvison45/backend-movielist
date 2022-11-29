6f91148eafa59a575b3d6146b8f7b8909e2b1b27
Dayvison <88006182+Dayvison45@users.noreply.github.com>
2022-11-29 18:06:27 -0300

    add files

---
+++ index.js
@@ -0,0 +1,15 @@
+const { default: axios } = require('axios')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+express()
+  .use(express.json())
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', async(req, res) =>{
+    const data = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=39113d47500d6a832e88e500d36006ec&language=en-US`)
+let newdata = data.data.results
+    res.json(newdata)})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
