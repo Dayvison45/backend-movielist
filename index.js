4603b3c9e8445d3164674bdbd229e8552e9ace6d
Dayvison <dayvisondapazpereira@outlook.com>
2022-11-29 19:02:51 -0300

    json add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
     await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=39113d47500d6a832e88e500d36006ec&language=en-US`).then(response=>respo.push(response.data)).catch(err=>console.log("nome errado")) 
     setTimeout(resposta,4000)
     function resposta(){
-      res.status(200).json(respo)
+      res.json(respo)
     }    
   
   })
