05a71a490dfec89bbab01b179eecb824a309c3ec
Dayvison <dayvisondapazpereira@outlook.com>
2022-11-30 08:27:12 -0300

    change node version

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const axios = require('axios')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3000
+import axios from 'axios'
+import  express  from 'express'
 
+const PORT = process.env.PORT || 3000
+import fetch from 'node-fetch'
 express()
   .use(express.json())
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', async(req, res) =>{
-    const respo = [] 
-    await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=39113d47500d6a832e88e500d36006ec&language=en-US`).then(response=>respo.push(response.data)).catch(err=>console.log("nome errado")) 
-    setTimeout(resposta,4000)
+    
+    const newdata = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=39113d47500d6a832e88e500d36006ec&language=en-US`)
+    setTimeout(resposta,1000)
     function resposta(){
-      res.json(respo)
+      console.log(newdata.data.results)
+      res.status(200).json(newdata.data.results)
     }    
   
   })
